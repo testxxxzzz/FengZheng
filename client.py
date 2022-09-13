44c2f9696570f03f9206b5a2cd9f8b655d0efa46
sshuo <924829024@qq.com>
2022-09-13 14:38:59 +0800

    2022.9.13

--- client.py
+++ client.py
@@ -1,6 +1,5 @@
 import socket
 import json
-from time import sleep
 
 
 def cserver():
@@ -14,43 +13,55 @@ def cserver():
 
 # 接收服务器端发送过来的数据
     res_msg = sk.recv(1024).decode()
-    print(res_msg)
+    #print(res_msg)
     dic_code = json.loads(res_msg)
     if dic_code["code"]:
-        print("恭喜你,登录成功")
+        print("welcome FengZheng")
         while True:
-            cmd = input("请输入命令\n")
+            cmd = input("------请输入命令------\n\n\n")
             cmd = str(cmd)
-            if cmd =="add":
+            if cmd == "add":
                 sk.send(bytes(cmd, encoding="utf_8"))
-                print("1111")
+                print("添加成功")
             elif cmd == 'getlist':
                 sk.send(bytes(cmd, encoding="utf_8"))
-                print("已执行")
+                print("\n已执行")
                 res_msg = sk.recv(1024).decode()
                 print(res_msg)
             elif cmd == 'del':
                 sk.send(bytes(cmd, encoding="utf_8"))
-                cmd2=input("请输入flag")
-                cmd3=str(cmd2)
+                cmd2 = input("请输入flag\n")
+                cmd3 = str(cmd2)
                 sk.send(bytes(cmd3, encoding="utf_8"))
-                print("已执行")
-            elif cmd=="exit":
-                print("3秒钟后退出")
-                
+                print("\n已执行")
+            elif cmd == 'alive':
                 sk.send(bytes(cmd, encoding="utf_8"))
-                sleep(3)
+                cmd2 = input("请输入要激活的flag\n")
+                cmd3 = str(cmd2)
+                sk.send(bytes(cmd3, encoding="utf_8"))
+                print("\n已执行")
+            elif cmd == "exit":
+                print("\n您已离开控制器")
+
+                sk.send(bytes(cmd, encoding="utf_8"))
+                #sleep(5)
                 sk.close()
                 break
-            elif cmd=="test":
+            elif cmd == "test":
                 sk.send(bytes(cmd, encoding="utf_8"))
                 res_msg = sk.recv(1024).decode()
                 print(res_msg)
                 # break
+            elif cmd == "help":
+                print("add:添加flag \n")
+                print("getlist:获取flag列表 \n")
+                print("del:删除flag \n")
+                print("exit:退出 \n")
+                print("test:测试 \n")
             else:
                 print("请输入help查看详细命令")
     else:
-        print("登录失败")
+        print("\n登录失败\n")
     sk.close()
 
 
--- teamserver.py
+++ teamserver.py
@@ -1,3 +1,6 @@
+from asyncio.windows_events import NULL
+from concurrent.futures import thread
+import datetime
 import os
 import random
 import shutil
@@ -5,13 +8,17 @@ import socket
 import json
 import http.server
 import socketserver
+import threading
+from time import time
 from tracemalloc import stop
 
 # flag = "X8KOy4"
-baseurl = "127.0.0.1"
 # flag=input("请输入URL：")
 # time=float (input("心跳时间："))
 # payload = "whoami"
+ts_pool = []
+baseurl = "127.0.0.1"  # teamserver服务启动地址
+HTTP_PORT = 8000  # http服务端口
 
 
 def generate_random_str(s=6):
@@ -22,13 +29,34 @@ def generate_random_str(s=6):
     return random_str
 
 
+# # 以server.py为模板，生成新的文件
+# def create(flag, time, payload, baseurl):
+#     with open("server.py", "r") as f:
+#         with open("server_"+flag+".py", "w") as g:
+#             for line in f.readlines():
+#                 if "flag=" in line:
+#                     line = "flag = \""+flag+"\"\n"
+#                 if "time=" in line:
+#                     line = "time = "+str(time)+"\n"
+#                 if "payload=" in line:
+#                     line = "payload = \""+payload+"\"\n"
+#                 if "baseurl=" in line:
+#                     line = "baseurl = \""+baseurl+"\"\n"
+#                 g.write(line)
+#     print("已生成新的server.py文件")
+
+
 def add():
     r = generate_random_str()
-
-    os.mkdir(r)
+    # 创建名为r的空文件
+    with open('./server/'+r, 'a+') as f:
+        f.write("")
+    print("已创建：", r)
     with open("log", 'a+') as f:
         f.write('flag:'+r+'    target:'+baseurl+'\n')
-    print("已创建：", r)
+    print("已写入log：", r)
+    # create(r, time, payload, baseurl)
+    # print("已生成新的server.py文件")
 
 
 def pwd():
@@ -36,105 +64,131 @@ def pwd():
     with open("pwd", 'w') as p:
         p.write(r2)
     print("您的TeamServer密码为：", r2, "请妥善保存！")
+# 激活server
 
 
-def delete(flag):
+def alive(flag):
     with open('log', 'r') as f:
         with open('log.bak', 'w') as g:
             for line in f.readlines():
                 if flag not in line:
                     g.write(line)
-        shutil.move('log.bak', 'log')
-    os.rename(flag, flag+"exit")
+    shutil.move('log.bak', 'log')
+    os.rename('./server/'+flag, './server/'+flag+"alive")
+    print("已激活：", flag)
 
-    print("已删除：")
+
+def delete(flag):
+    # 检测文件是否存在
+    if os.path.exists('./server/'+flag):
+        os.remove('./server/'+flag)
+        print("已删除未激活的server：", flag)
+    elif os.path.exists('./server/'+flag+"alive"):
+        # 删除server文件夹下的flag文件
+        os.remove('./server/'+flag+"alive")
+        print("已删除：", flag)
+    else:
+        print("没有存在的server，可能输入了异常字符！")
 
 
 def getlist():
     with open("log", "r") as f:
         txt = f.read()
-    # print(txt)
+        if txt == "":
+            return "暂无数据"
     return txt
 
+
 def httpserver():
-    PORT = 8000
+
     Handler = http.server.SimpleHTTPRequestHandler
-    with socketserver.TCPServer(("", PORT), Handler) as httpd:
-        print("serving at port", PORT)
+    with socketserver.TCPServer(("", HTTP_PORT), Handler) as httpd:
+        print("HTTP server port", HTTP_PORT)
         httpd.serve_forever()
 
 
-def tserver():
+def command(conn):
+    while True:
+        msg = conn.recv(1024).decode()
+        if msg == "getlist":
+            lists = getlist()
+            conn.send(bytes(lists.encode()))
+            # 清空msg
+            msg = ""
+            continue
+        elif msg == "add":
+            add()
+            msg = ""
+            continue
+        elif msg == "del":
+
+            msg2 = conn.recv(1024).decode()
+            print(msg2)
+            delete(str(msg2))
+            msg = ""
+            continue
+        elif msg == "test":
+            def excmd():
+                if msg == "test":
+
+                    conn.send(bytes("server---ok".encode()))
+                    return "client---ok"
+            excmd()
+            msg = ""
+            continue
+        elif msg == "alive":
+            msg2 = conn.recv(1024).decode()
+
+            alive(msg2)
+            msg = ""
+            continue
+
+        elif msg == "exit":
+            print("一个用户退出了服务器", datetime.datetime.now())
+            conn.close()
+            break
+
+
+def tserver(addr):
     sk = socket.socket()
-    sk.bind(("127.0.0.1", 9000))
+    sk.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sk.bind((addr, 9000))
+
     print("正在启动服务器......\n")
     sk.listen()
+
     print("启动成功")
-    #pwd() #随机生成密码
-    conn, addr = sk.accept()
-    msg = conn.recv(1024).decode()
-    sign = False
-    with open("pwd", "r") as fp:
-        for line in fp:
-            lpwd = line
-            if msg == lpwd:
-                res = {"code": 1}
-                res_msg = json.dumps(res).encode()
-                conn.send(res_msg)
-                sign = True
-            if sign == False:
-                res = {"code": 0}
-                print(res)
-                res_msg = json.dumps(res).encode()
-                conn.send(res_msg)
-            while True:
-                # try:
-                msg = conn.recv(1024).decode()
-                # except:
-                #     pass
-                # else:
-                #     print("位置三：",msg)
-                #     msg="exit"
-                #     break
-                
-                while True:
-                    if msg == "getlist":
-                        lists=getlist()
-                        conn.send(bytes( lists.encode()))
-                    elif msg == "add":
-                        add()
-                        break
-                    elif msg == "del":
-                        # print("请输入目标值")  ##删除功能需要修改
-                        msg2 = conn.recv(1024).decode()
-                        print(msg2)
-                        delete(str(msg2))
-                        break
-                    elif msg == "test":
-                        def excmd():
-                            if msg == "test":
-                                print("ok")
-                                conn.send(bytes( "ok".encode()))
-                                return "ok"
-                        excmd()
-                        break
-                    elif msg == "exit":
-                        print("11111111111111")
-                        conn.close()
-                        print("22222222222")
-                        # sk.close()
-                        # break
-                        # continue
-                                     
-                        #！！！！！！！！！解决客户端终止后无法再次连接的问题
-                    # break
-                    continue
+    pwd()  # 随机生成密码
+    while True:
+
+        conn, addr = sk.accept()
+
+        msg = conn.recv(1024).decode()
+        sign = False
+        with open("pwd", "r") as fp:
+            for line in fp:
+                lpwd = line
+                if msg != lpwd:
+                    sign == False
+                    res = {"code": 0}
+                    res_msg = json.dumps(res).encode()
+                    conn.send(res_msg)
+                    break
+                else:
+                    res = {"code": 1}
+
+                    res_msg = json.dumps(res).encode()
+                    conn.send(res_msg)
+                    sign = True
+                    print("一个用户登录了服务器", datetime.datetime.now())
+
+                ts_pool.append(conn)
+                thread = threading.Thread(target=command, args=(conn,))
+                thread.setDaemon(True)
+                thread.start()
 
 
-# add()
-# delete()
-# getlist()
-# pwd()
-# httpserver()
-tserver()
+t = threading.Thread(target=tserver, args=(baseurl,))
 
+t.start()
+httpserver()
